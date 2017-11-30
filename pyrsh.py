d1159337809d482d115d7dff6bb4f8b846e0697d
Sergey Bardeniuk <Serg.websearch@gmail.com>
2017-11-30 18:38:57 +0200

    add context manager

--- pyrsh.py
+++ pyrsh.py
@@ -2,42 +2,10 @@
 # -*- coding: utf-8 -*-
 import paramiko
 import argparse
+import sys
+from contextlib import contextmanager
 	
-def main(args):
-	host = args.host
-	user = args.user
-	psw = args.password
-	port = args.port
-
-	try:
-		ssh = paramiko.SSHClient()
-
-		# If you can not find the key when connecting to the server, then by default the key is "discouraged" and called SSHException.
-		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		
-		ssh.connect(hostname=host, port=port, username=user, password=psw)
-
-		# implementation commands
-		stdin, stdout, stderr = ssh.exec_command(args.command)
-
-		# Output the result
-		result = stdout.read() + stderr.read()
-		print(result)
-
-		# For output the list
-		# result = stdout.read().splitlines()
-		# print(result)
-
-		# close connection
-		ssh.close()
-		pass
-	except:
-		print('Connection failed')
-		pass
-
-
-
-if __name__ == "__main__":
+def arguments():
 	# Create description scrit
 	parser = argparse.ArgumentParser(description='Remote command execution via ssh')
 	#  specify the name and description argyments
@@ -46,6 +14,43 @@ if __name__ == "__main__":
 	parser.add_argument('password', help='Your password', type=str)
 	parser.add_argument('command', help='Your command', type=str)
 	parser.add_argument('port', nargs='?', default=22, help='Enter your port, default port=22', type=int)
-	args = parser.parse_args()
+	result = parser.parse_args()
+	return result
 	
-	main(args)
+@contextmanager
+def ssh(args):
+	try:
+		connect = paramiko.SSHClient()
+		# If you can not find the key when connecting to the server, then by default the key is "discouraged" and called SSHException.
+		connect.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		connect.connect(hostname=args.host, port=args.port, username=args.user, password=args.password)
+	except paramiko.ssh_exception.AuthenticationException:
+		sys.stderr.write("Authentication failed.\n")
+		sys.exit(-1)
+
+	try: 
+		if connect:
+			yield connect
+		else:
+			yield False
+	finally:
+		if connect:
+			# close connection
+			connect.close()
+
+def main():
+	args = arguments()
+	try:
+		with ssh(args) as connect:
+			# implementation commands
+			stdin, stdout, stderr = connect.exec_command(args.command)
+			# Output the result
+			result = stdout.read() + stderr.read()
+			print(result)
+	except:
+		sys.stderr.write("Encountered an error\n")
+		sys.exit(-1)
+
+	
+if __name__ == "__main__":
+	main()
