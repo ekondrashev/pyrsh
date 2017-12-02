adbb8587a83275b89d4fcf461b0bdc76dfa83ef9
Sergey Bardeniuk <Serg.websearch@gmail.com>
2017-12-02 10:57:12 +0200

    remove if/else

--- pyrsh.py
+++ pyrsh.py
@@ -4,11 +4,10 @@ import paramiko
 import argparse
 import sys
 from contextlib import contextmanager
-	
+
+
 def arguments():
-	# Create description scrit
 	parser = argparse.ArgumentParser(description='Remote command execution via ssh')
-	#  specify the name and description argyments
 	parser.add_argument('host', help='Hostname', type=str)
 	parser.add_argument('user', help='Your user', type=str)
 	parser.add_argument('password', help='Your password', type=str)
@@ -21,7 +20,6 @@ def arguments():
 def ssh(args):
 	try:
 		connect = paramiko.SSHClient()
-		# If you can not find the key when connecting to the server, then by default the key is "discouraged" and called SSHException.
 		connect.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 		connect.connect(hostname=args.host, port=args.port, username=args.user, password=args.password)
 	except paramiko.ssh_exception.AuthenticationException:
@@ -29,22 +27,15 @@ def ssh(args):
 		sys.exit(-1)
 
 	try: 
-		if connect:
-			yield connect
-		else:
-			yield False
+		yield connect
 	finally:
-		if connect:
-			# close connection
-			connect.close()
+		connect.close()
 
 def main():
 	args = arguments()
 	try:
 		with ssh(args) as connect:
-			# implementation commands
 			stdin, stdout, stderr = connect.exec_command(args.command)
-			# Output the result
 			result = stdout.read() + stderr.read()
 			print(result)
 	except:
