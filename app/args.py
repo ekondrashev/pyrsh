9931a865ecee84b168f9d37a54e751469535c0f2
Sergey Bardeniuk <Serg.websearch@gmail.com>
2017-12-19 09:17:28 +0200

    Create args.py

---
+++ app/args.py
@@ -0,0 +1,8 @@
+class Arguments(object):
+    def __init__(self, args):
+        self.type = args.type
+        self.cmd = args.cmd
+        self.user = args.user
+        self.password = args.password
+        self.host = args.host
+        self.port = args.port
