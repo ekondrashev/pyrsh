1492fe762fa329b92bf3684ddd5a06e944eb2ab1
Sergey Bardeniuk <Serg.websearch@gmail.com>
2017-12-19 08:47:25 +0200

    Update paramiko.py

--- app/paramiko.py
+++ app/paramiko.py
@@ -2,40 +2,36 @@ import paramiko
 import time
 from contextlib import contextmanager
 
-class paramiko(object):
-	def __init__(self, args):
-		self.host = args.host
-		self.user = args.user
-		self.password = args.password
-		self.cmd = args.cmd
-		self.port = args.port
+class ClParamiko(object):
+    def __init__(self, args):
+        self.args = args
 
-	@contextmanager
-	def connect(self):
-		con = paramiko.SSHClient()
-		con.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		con.connect(hostname=self.host, port=self.port, username=self.user, password=self.password)
-		yield con
-		con.close()
+    @contextmanager
+    def connect(self):
+        con = paramiko.SSHClient()
+        con.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        con.connect(hostname=self.args.host, port=self.args.port, username=self.args.user, password=self.args.password)
+        yield con
+        con.close()
 
-	def response(self, channel):
-		while not channel.recv_ready():
-			time.sleep(0.1)
-		stdout = ''
-		while channel.recv_ready():
-			stdout += channel.recv(1024)
-		return stdout
+    def response(self, channel):
+        while not channel.recv_ready():
+            time.sleep(0.1)
+        stdout = ''
+        while channel.recv_ready():
+            stdout += channel.recv(1024)
+        return stdout
 
-	def sendncon(self):
-		with self.connect() as connect:
-			channel = connect.invoke_shell()
-			self.response(channel)
-			channel.send(self.cmd+'\n')
-			stdout = self.response(channel)
-			return stdout
+    def send_connect(self):
+        with self.connect() as connect:
+            channel = connect.invoke_shell()
+            self.response(channel)
+            channel.send(self.args.cmd+'\n')
+            stdout = self.response(channel)
+            return stdout
 
-	def run(self):
-		try:
-			return(self.sendncon())
-		except paramiko.ssh_exception.AuthenticationException as e:
-			return(e)
+    def run(self):
+        try:
+            return(self.send_connect())
+        except paramiko.ssh_exception.AuthenticationException as e:
+            return(e)
