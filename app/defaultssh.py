b3cd94f0f503aa7f67be6ff80bc82defff7b9542
Sergey Bardeniuk <Serg.websearch@gmail.com>
2017-12-21 11:46:21 +0200

    Update defaultssh.py

--- app/defaultssh.py
+++ app/defaultssh.py
@@ -3,18 +3,18 @@ import subprocess
 from args import Arguments
 
 class DefaultSsh(Arguments):
-  def __init__(self, args):
-    Arguments.__init__(self, args)
+    def __init__(self, args):
+        Arguments.__init__(self, args)
 
-  def run(self):
-    per = str(self.user+'@'+self.host)
-    ssh = subprocess.Popen(['ssh', per, self.cmd],
+    def run(self):
+        per = str(self.user+'@'+self.host)
+        ssh = subprocess.Popen(['ssh', per, self.cmd],
                 shell=False,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE)
-    result = ssh.stdout.readlines()
-    if result == []:
-      # error
-      return ssh.stderr.readlines()
-    else:
-      return result
+        result = ssh.stdout.readlines()
+        if result == []:
+        # error
+            return ssh.stderr.readlines()
+        else:
+            return result
