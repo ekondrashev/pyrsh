7964834fdc8773d2ab8e5b229bf39a6a72c81541
Sergey Bardeniuk <Serg.websearch@gmail.com>
2017-12-26 08:51:29 +0200

    Update pyrsh.py

--- pyrsh.py
+++ pyrsh.py
@@ -13,7 +13,6 @@ def arguments():
     parser.add_argument('type', help='Type remote connection. Value: local - local execute cmd; ssh - run a command for Linux without using third-party libraries; telnet - run a command for Linux without using third-party libraries; paramiko - run a command with using paramiko', type=str)
     parser.add_argument('cmd', help='Command bash', type=str)
     parser.add_argument('user', help='User name to use for authentication', type=str)
-    parser.add_argument('password', help='Password to use for authentication', type=str)
     parser.add_argument('host', nargs='?', default="127.0.0.1", help='Hostname to use for authentication', type=str)
     parser.add_argument('port', nargs='?', default=22, help='Port/ID, default port=22', type=int)
     return parser.parse_args()
@@ -27,6 +26,8 @@ def main():
                     'paramiko': ClParamiko,
                 }
         args = arguments()
+        password = raw_input('Enter password: ')
+        setattr(args, 'password', password)
         cl  = dicar.get(args.type)(args) 
         print(cl.run())
     except:
