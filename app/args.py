5a798bc6dbb78d05c17eb670c5f4af874afda90b
Sergey Bardeniuk <Serg.websearch@gmail.com>
2017-12-26 08:53:42 +0200

    Update args.py

--- app/args.py
+++ app/args.py
@@ -1,5 +1,12 @@
-class Arguments(object):
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from basecheck import CheckMethod
+
+class Arguments(CheckMethod):
+
     def __init__(self, args):
+    	CheckMethod.__init__(self)
         self.type = args.type
         self.cmd = args.cmd
         self.user = args.user
