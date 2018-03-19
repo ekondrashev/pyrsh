c4436c3dbc547c60a40054b991d1d4e3d49b181c
Sergey Bardeniuk <Serg.websearch@gmail.com>
2018-03-19 20:28:30 +0200

    Update local.py

--- app/local.py
+++ app/local.py
@@ -1,8 +1,9 @@
 import subprocess
 
 from args import Arguments
+from shell import Shell
 
-class Local(Arguments):
+class Local(Arguments, Shell):
     def __init__(self, args):
         Arguments.__init__(self, args)
 
