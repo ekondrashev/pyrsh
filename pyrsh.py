86a82c66a0bb16808c1e66c653fb753b1c1dbe07
Sergey Bardeniuk <Serg.websearch@gmail.com>
2018-03-20 07:45:53 +0200

    Update pyrsh.py

--- pyrsh.py
+++ pyrsh.py
@@ -13,7 +13,6 @@ def arguments():
     parser.add_argument('type', help='Type remote connection. Value: local - local execute cmd; ssh - run a command for Linux without using third-party libraries; telnet - run a command for Linux without using third-party libraries; paramiko - run a command with using paramiko', type=str)
     parser.add_argument('cmd', help='Command bash', type=str)
     parser.add_argument('user', help='User name to use for authentication', type=str)
-    parser.add_argument('password', help='Password to use for authentication', type=str)
     parser.add_argument('host', nargs='?', default="127.0.0.1", help='Hostname to use for authentication', type=str)
     parser.add_argument('port', nargs='?', default=22, help='Port/ID, default port=22', type=int)
     return parser.parse_args()
@@ -27,7 +26,8 @@ def main():
                     'paramiko': ClParamiko,
                 }
         args = arguments()
-        cl  = dicar.get(args.type)(args) 
+        get_password = raw_input("Enter the Authentication Password")
+        cl  = dicar.get(args.type)(args, get_password) 
         try:
             print(cl.run())
         except NotImplementedError, msg:
