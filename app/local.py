04ee090bd0d855d12992e5212f070e591385a98e
Sergey Bardeniuk <Serg.websearch@gmail.com>
2018-03-20 07:46:03 +0200

    Update local.py

--- app/local.py
+++ app/local.py
@@ -4,8 +4,8 @@ from args import Arguments
 from shell import Shell
 
 class Local(Arguments, Shell):
-    def __init__(self, args):
-        Arguments.__init__(self, args)
+    def __init__(self, args, password):
+        Arguments.__init__(self, args, password)
 
     def run(self):
         return subprocess.Popen(self.cmd, shell=True)
