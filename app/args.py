23ce1d307a504391453e29f33aba421c8e9e92a1
Sergey Bardeniuk <Serg.websearch@gmail.com>
2018-03-20 07:45:56 +0200

    Update args.py

--- app/args.py
+++ app/args.py
@@ -1,8 +1,8 @@
 class Arguments(object):
-    def __init__(self, args):
+    def __init__(self, args, password):
         self.type = args.type
         self.cmd = args.cmd
         self.user = args.user
-        self.password = args.password
+        self.password = password
         self.host = args.host
         self.port = args.port
