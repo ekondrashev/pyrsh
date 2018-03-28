82c8c8923839dba46b3d66cd85f11df8557172f4
Sergey Bardeniuk <Serg.websearch@gmail.com>
2018-03-28 14:48:59 +0300

    Update telnet.py

--- app/telnet.py
+++ app/telnet.py
@@ -3,14 +3,13 @@ import time
 from contextlib import contextmanager
 
 from args import Arguments
-from shell import Shell
 
-class ClTelnet(Arguments, Shell):
-    def __init__(self, args, password):
-        Arguments.__init__(self, args, password)
+class ClTelnet(Arguments):
+    def __init__(self, args):
+        Arguments.__init__(self, args)
 
     @contextmanager
-    def tn_connect(self):
+    def _tn_connect(self):
         tn = telnetlib.Telnet(self.host, self.port)
         tn.read_until("login: ")
         tn.write(self.user + "\r\n")
@@ -20,7 +19,10 @@ class ClTelnet(Arguments, Shell):
         tn.close()
 
     def run(self):
-        with self.tn_connect() as connect:          
-            tn.write(self.cmd + "\r\n")
-            time.sleep(1)
-            return tn.read_all()
+        with self._tn_connect() as connect: 
+            
+            for cmd in self.cmds:
+                tn.write(self.cmd + "\r\n")
+                time.sleep(1)
+                self.result[cmd] = tn.read_all()
+            return self.result  
