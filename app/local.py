0240609743bbbcce68625074207c60da56d70382
Sergey Bardeniuk <Serg.websearch@gmail.com>
2018-03-28 14:49:21 +0300

    Update local.py

--- app/local.py
+++ app/local.py
@@ -1,11 +1,14 @@
 import subprocess
 
 from args import Arguments
-from shell import Shell
 
-class Local(Arguments, Shell):
-    def __init__(self, args, password):
-        Arguments.__init__(self, args, password)
+
+class Local(Arguments):
+    def __init__(self, args):
+        Arguments.__init__(self, args)
 
     def run(self):
-        return subprocess.Popen(self.cmd, shell=True)
+        for cmd in self.cmds:
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            self.result[cmd] = p.stdout.read()
+        return self.result
