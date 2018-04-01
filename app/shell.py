11577c02325514051712a1230f617f3701150879
Sergey Bardeniuk <Serg.websearch@gmail.com>
2018-04-01 20:55:27 +0300

    Update shell.py

--- app/shell.py
+++ app/shell.py
@@ -1,3 +1,10 @@
 class Shell(object):
+    def __init__(self, args):
+        self.type = args.type
+        self.user = args.user
+        self.password = args.password
+        self.host = args.host
+        self.port = args.port
+
     def run(self):
         raise NotImplementedError('Abstract method not implemented.')
