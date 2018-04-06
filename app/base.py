9a6a1ad1e0245d3e76f52f5483c63104e86ca5ef
Sergey Bardeniuk <Serg.websearch@gmail.com>
2018-04-06 11:42:17 +0300

    Update base.py

--- app/base.py
+++ app/base.py
@@ -1,35 +1,32 @@
-import paramiko as p
-import time
-from contextlib import contextmanager
-import subprocess
-import telnetlib
++import paramiko
++import time
++from contextlib import contextmanager
++import subprocess
++import telnetlib
 
 class Base(object):
     def __init__(self, args):
         self.type = args.type
+        self.cmd = args.cmd.split('#')
         self.user = args.user
-        # self.password = args.password
-        self.password = "MndJa1hkPimLDZya"
+        self.password = args.password
         self.host = args.host
         self.port = args.port
 
+    def run(self):
+        raise NotImplementedError('Abstract method not implemented.')
+
+class Paramiko(Base):
+    def __init__(self, args):
+        Base.__init__(self, args)
+
     @contextmanager
     def _connect(self):
-        con = p.SSHClient()
-        con.set_missing_host_key_policy(p.AutoAddPolicy())
+        con = paramiko.SSHClient()
+        con.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         con.connect(hostname=self.host, port=self.port, username=self.user, password=self.password)
         yield con
         con.close()
-
-    @contextmanager
-    def _tn_connect(self):
-        tn = telnetlib.Telnet(self.host, self.port)
-        tn.read_until("login: ")
-        tn.write(self.user + "\r\n")
-        tn.read_until("password: ")
-        tn.write(self.password + "\r\n")
-        yield tn
-        tn.close()
 
     def _response(self, channel):
         while not channel.recv_ready():
@@ -38,7 +35,7 @@ class Base(object):
         while channel.recv_ready():
             stdout += channel.recv(1024)
         return stdout
-    
+
     def _send_connect(self, cmd):
         with self._connect() as connect:
             channel = connect.invoke_shell()
@@ -46,23 +43,26 @@ class Base(object):
             channel.send(cmd+'\n')
             return self._response(channel)
 
-    def paramiko(self, cmd):
+    def run(self, cmd):
         try:
             return(self._send_connect(cmd))
-        except p.ssh_exception.AuthenticationException as e:
+        except paramiko.ssh_exception.AuthenticationException as e:
             return(e)
 
-    def local(self, cmd):
-        res = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-        return res.stdout.read()
+class Local(Base):
+    def __init__(self, args):
+        Base.__init__(self, args)
 
-    def telnet(self, cmd):
-        with self._tn_connect() as connect: 
-            tn.write(cmd + "\r\n")
-            time.sleep(1)
-            return tn.read_all()
+    def run(self, cmd):
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        return p.stdout.read()
 
-    def ssh(self, cmd):
+
+class Ssh(Base):
+    def __init__(self, args):
+        Base.__init__(self, args)
+
+    def run(self, cmd):
         per = str(self.user+'@'+self.host)
         ssh = subprocess.Popen(['ssh', per, cmd],
                 shell=False,
@@ -74,11 +74,22 @@ class Base(object):
         else:
             return checkerror
 
+class Telnet(Base):
+    def __init__(self, args):
+        Base.__init__(self, args)
+
+    @contextmanager
+    def _tn_connect(self):
+        tn = telnetlib.Telnet(self.host, self.port)
+        tn.read_until("login: ")
+        tn.write(self.user + "\r\n")
+        tn.read_until("password: ")
+        tn.write(self.password + "\r\n")
+        yield tn
+        tn.close()
+
     def run(self, cmd):
-        dicar = {
-                'local': self.local,
-                'ssh': self.ssh,
-                'telnet': self.telnet,
-                'paramiko': self.paramiko,
-            }
-        return dicar.get(self.type)(cmd)
+        with self._tn_connect() as connect: 
+            tn.write(cmd + "\r\n")
+            time.sleep(1)
+            return tn.read_all()
