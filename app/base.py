28055b21fa326568299ffc98720eab89b0f5c31a
Sergey Bardeniuk <Serg.websearch@gmail.com>
2018-04-10 21:34:59 +0300

    Update base.py

--- app/base.py
+++ app/base.py
@@ -1,30 +1,43 @@
-+import paramiko
-+import time
-+from contextlib import contextmanager
-+import subprocess
-+import telnetlib
+import paramiko
+import time
+from contextlib import contextmanager
+import subprocess
+import telnetlib
 
 class Base(object):
     def __init__(self, args):
         self.type = args.type
         self.cmd = args.cmd.split('#')
         self.user = args.user
-        self.password = args.password
+        # self.password = args.password
+        self.password = "MndJa1hkPimLDZya"
         self.host = args.host
         self.port = args.port
 
-    def run(self):
+    def _execution(self, per):
+        ssh = subprocess.Popen(per,
+                shell=False,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+                )
+        checkerror = ssh.stdout.readlines()
+        if checkerror == []:
+            return ssh.stderr.readlines()
+        else:
+            return checkerror
+
+    def run(self, *args, **kwargs):
         raise NotImplementedError('Abstract method not implemented.')
 
 class Paramiko(Base):
     def __init__(self, args):
-        Base.__init__(self, args)
+        self.arg = Base(args)
 
     @contextmanager
     def _connect(self):
         con = paramiko.SSHClient()
         con.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        con.connect(hostname=self.host, port=self.port, username=self.user, password=self.password)
+        con.connect(hostname=self.arg.host, port=self.arg.port, username=self.arg.user, password=self.arg.password)
         yield con
         con.close()
 
@@ -49,42 +62,31 @@ class Paramiko(Base):
         except paramiko.ssh_exception.AuthenticationException as e:
             return(e)
 
+
+
 class Local(Base):
-    def __init__(self, args):
-        Base.__init__(self, args)
-
     def run(self, cmd):
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-        return p.stdout.read()
-
+        return self._execution(cmd)
 
 class Ssh(Base):
     def __init__(self, args):
-        Base.__init__(self, args)
+        self.arg = Base(args)
 
     def run(self, cmd):
-        per = str(self.user+'@'+self.host)
-        ssh = subprocess.Popen(['ssh', per, cmd],
-                shell=False,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE)
-        checkerror = ssh.stdout.readlines()
-        if checkerror == []:
-            return ssh.stderr.readlines()
-        else:
-            return checkerror
+        per = str(self.arg.user+'@'+self.arg.host)
+        return self._execution(['ssh', per, cmd])
 
 class Telnet(Base):
     def __init__(self, args):
-        Base.__init__(self, args)
+        self.arg = Base(args)
 
     @contextmanager
     def _tn_connect(self):
-        tn = telnetlib.Telnet(self.host, self.port)
+        tn = telnetlib.Telnet(self.arg.host, self.arg.port)
         tn.read_until("login: ")
-        tn.write(self.user + "\r\n")
+        tn.write(self.arg.user + "\r\n")
         tn.read_until("password: ")
-        tn.write(self.password + "\r\n")
+        tn.write(self.arg.password + "\r\n")
         yield tn
         tn.close()
 
