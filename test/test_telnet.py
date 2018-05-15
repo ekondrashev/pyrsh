68e4753a3baed3f197e7103c3a6e3b70d2eb0615
Sergey Bardeniuk <Serg.websearch@gmail.com>
2018-05-15 22:13:40 +0300

    Update test_telnet.py

--- test/test_telnet.py
+++ test/test_telnet.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import unittest
 from argparse import Namespace
 
-from app.telnet import ClTelnet
+from shell.base import Telnet
 
 
 def args(cmd, host='localhost', user='a', password='a', port=10023):
@@ -15,9 +15,17 @@ def args(cmd, host='localhost', user='a', password='a', port=10023):
 
 
 class TelnetTest(unittest.TestCase):
+    def setUp(self):
+        self.shell = Telnet(args("cmd"))
+        self.shell = self.shell.__enter__()
+
+    def tearDown(self):
+        self.shell.__exit__(self, 'var1', 'var1',)
+        self.assertTrue(1)
+
+
     def test_telnet(self):
-        res = ClTelnet(args("version"))
-        self.assertEqual(res.run(), ('V1.0'))
+        self.assertEqual(self.shell.run("version"), ('V1.0'))
 
 if __name__ == "__main__":
     unittest.main()
